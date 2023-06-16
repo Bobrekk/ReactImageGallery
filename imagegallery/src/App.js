901825e14cf5b4870449c848b7f2cc359900e645
Bobrekk <81027034+Bobrekk@users.noreply.github.com>
2023-06-16 23:31:11 +0300

    Add files via upload

---
+++ imagegallery/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Gallery from './Gallery'
+
+function App() {
+  return (
+    <>
+      <h1 className='mainH1'>Image Gallery</h1>
+      <Gallery/>
+    </>
+  );
+}
+
+export default App;
